b7a66cac4311c88c0cf063eb70a3c3295a57e2ef
Rodolfo Silva <100287751+Jrodolfosilva@users.noreply.github.com>
2022-09-01 16:00:22 -0300

    Atualização dos métodos
    Ajuste na estrutura dos métodos e criação de validações para as requisições.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const banco =[
     {
-        id:1,
+        id:"1",
         name:"Rodolfo"
     },
     {
-        id:2,
+        id:"2",
         name:"Rodolfo"
     }
 ]
@@ -18,28 +18,41 @@ app.get("/",(req,resp)=>{
 resp.send(
   banco
 )
-})
-app.post("/post",(req,resp)=>{
-const {id,name} = req.body
-banco.push({
-    id,
-    name})
+});
 
-resp.status(200).send(
-    banco
-)
-})
+app.post("/post",(req,resp)=>{
+
+    const {id,name} = req.body
+    if(banco.find((pes)=>pes.name === name || pes.id === id)){
+        resp.status(500).send("Usuario já existe")
+    }
+    else{
+        banco.push({
+        id,
+        name})
+        resp.status(200).send("Cadastrado com sucesso!!")
+    }
+});
+
 
 app.patch("/patch",(req,resp)=>{
-    const {id,name} = req.body;
 
-    banco.find((i)=>i.id === id)?
-     resp.status(500).send("Já está cadastrado"):
-     banco.push({id,
-        name})&&resp.send(banco)
+const {id,name} = req.body;
+if(!banco.find((pes)=>pes.id === id)){
+    resp.status(404).send("Dado não encontrado")
+}
 
+const pessoa = banco.find((pes)=>pes.id === id)
+
+pessoa.id = pessoa.id
+pessoa.name = name? name: pessoa.name
+resp.status(200).send("Atualizado!!")
+/*fazer a lógica da atualização
+verificar se tem id no banco e tem atualizar o name
+se não retornar erro*/
 
 
 })
+
 
 app.listen(5000)
